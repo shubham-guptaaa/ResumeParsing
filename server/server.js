28027375ea4df014a7ed05c537c1109949dcc0ab
shubham-guptaaa <shubhamguptaa70@gmail.com>
2026-04-16 09:20:28 +0530

    server.js

--- server/server.js
+++ server/server.js
@@ -1,13 +1,6 @@
-import "dotenv/config";
 import { app } from "./src/app.js";
 
-const PORT = process.env.PORT || 3001;
 
-if (!process.env.VERCEL) {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
 
 export default app;
 
